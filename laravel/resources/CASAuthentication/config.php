1d482f6a5d9f231e00c8cbaa62650cd342c7260f
Wesley Haws <Wes@wess-mbp.wifi.it.et.byu.edu>
2016-02-01 08:16:33 -0700

    change the folder path for fixing.

--- laravel/resources/CASAuthentication/config.php
+++ laravel/resources/CASAuthentication/config.php
@@ -17,7 +17,7 @@
  * @link     https://wiki.jasig.org/display/CASC/phpCAS
  */
 
-$phpcas_path = 'CASAuthentication';
+$phpcas_path = '../resources/CASAuthentication';
 
 ///////////////////////////////////////
 // Basic Config of the phpCAS client //
