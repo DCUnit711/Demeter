9327c4b83ead0319017aad4c5dbade2af4bca581
Wesley Haws <Wes@Wess-MacBook-Pro.local>
2016-02-08 20:17:51 -0700

    cas

--- laravel/public/demeter/elements/dem-login/dem-login.js
+++ laravel/public/demeter/elements/dem-login/dem-login.js
@@ -42,7 +42,7 @@ Polymer({
 	},
 	login:function(){
 		this.hideToolbar = false;
-		var url = window.location.origin+"/demeter/CASLogic.php";
+		var url = window.location.origin+"/CASLogic.php";
 		window.location.href = url;
 	},
 	ajaxCASVerifyResponse:function(){
