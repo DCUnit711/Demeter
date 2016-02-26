784be5dbd67a53d9ee0aa6951e5ba2c5ab08c67b
Wesley Haws <Wes@wess-mbp.wifi.it.et.byu.edu>
2016-02-25 20:20:58 -0700

    log

--- laravel/public/demeter/elements/dem-edit/dem-edit.js
+++ laravel/public/demeter/elements/dem-edit/dem-edit.js
@@ -59,7 +59,7 @@ Polymer({
 										'organization':this.inputOrg,
 										'maxSize':this.inputSize,
 										'ownerId':this.inputOwner};
-
+		console.log(this.$.ajaxSendChanges.generateRequest())
 		this.$.ajaxSendChanges.generateRequest();
 	}
 });
