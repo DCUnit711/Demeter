18fc6aef418da990d5488b32d550f3864ded6f30
Wesley Haws <Wes@wess-mbp.wifi.it.et.byu.edu>
2016-02-26 08:25:01 -0700

    adding a new database functionality

--- laravel/public/demeter/elements/dem-create/dem-create.js
+++ laravel/public/demeter/elements/dem-create/dem-create.js
@@ -19,9 +19,20 @@ Polymer({
 	    }
 	},
 	selectSize:function(e){
-		console.log('selecting size');
 		this.$.sizeDropdown.label = e.target._lightChildren[0].data;
-		console.log(e.target._lightChildren[0].data);
+		switch(e.target._lightChildren[0].data) {
+			case "2 GB":
+				this.size = 2048;
+				break;
+			case "4 GB":
+				this.size = 4096;
+				break;
+			case "8 GB":
+				this.size = 8184;
+				break;
+			default:
+				break;
+		}
 	},
 	selectType:function(e){
 		this.$.typeDropdown.label = e.target._lightChildren[0].data;
@@ -31,10 +42,16 @@ Polymer({
 			this.inputOwner = this.database.OWNERID;
 		}
 		if(this.inputName == null || this.inputName == "") {
-
+			this.inputName = this.database.NAME;
 		}
 		if(this.inputDesc == null || this.inputDesc == "") {
-
+			this.inputDesc = this.database.DESCRIPTION;
+		}
+		if(this.inputOrg == null || this.inputOrg == "") {
+			this.inputOrg = this.database.ORGANIZATION;
+		}
+		if(this.size == undefined || this.size < 1) {
+			console.error('didnt add size');
 		}
 		var xhttp = new XMLHttpRequest();
 		xhttp.onreadystatechange = function() {
@@ -43,10 +60,10 @@ Polymer({
 		    }
 		};
 		var body = JSON.stringify({'name':this.inputName,
-								   'ownerId':this.database.OWNERID,
+								   'ownerId':this.inputOwner,
 								   'description':this.inputDesc,
 								   'organization':this.inputOrg,
-								   'maxSize':parseFloat(this.inputSize)});
+								   'maxSize':parseFloat(this.size)});
 
 		var url = "/instances/"+this.database.ID;
 		xhttp.open("PUT", url, true);
