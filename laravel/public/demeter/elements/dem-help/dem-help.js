0ff3612bce6eed8c52350e04bcb0bc0a6e947030
Wesley Haws <Wes@Wess-MBP.app.byu.edu>
2016-03-24 20:11:32 -0600

    update syntax error

--- laravel/public/demeter/elements/dem-help/dem-help.js
+++ laravel/public/demeter/elements/dem-help/dem-help.js
@@ -21,7 +21,7 @@ Polymer({
 		this.externalLinks = [{'link':'https://dev.mysql.com/doc/refman/5.5/en/connecting.html', 'description':'MySQL - How To Log Into and query the database?'},
 							  {'link':'http://dev.mysql.com/doc/refman/5.7/en/create-user.html','description':'MySQL - How Do I Create A User?'},
 							  {'link':'http://dev.mysql.com/doc/refman/5.7/en/update.html','description':'MySQL - How Do I Update Information?'},
-							  {'link':'http://dev.mysql.com/doc/refman/5.7/en/delete.html','description';'MySQL - How Do I Delete Information?'},
+							  {'link':'http://dev.mysql.com/doc/refman/5.7/en/delete.html','description':'MySQL - How Do I Delete Information?'},
 							  {'link':'http://dev.mysql.com/doc/refman/5.7/en/insert.html','description':'MySQL - How Do I Insert New Information'}];
 	},
 	goToLink:function(e){
