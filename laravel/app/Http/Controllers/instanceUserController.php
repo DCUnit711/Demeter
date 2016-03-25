847ea5453579dc38d773cf47c4ed862acb64c1ad
Robert Jackson <robert@thosejacksons.com>
2016-03-25 08:58:18 -0600

    fix

--- laravel/app/Http/Controllers/instanceUserController.php
+++ laravel/app/Http/Controllers/instanceUserController.php
@@ -8,6 +8,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\instanceUser;
 use App\instance;
+use App\instanceUser;
 use Illuminate\Http\Response;
 
 class instanceUserController extends Controller
