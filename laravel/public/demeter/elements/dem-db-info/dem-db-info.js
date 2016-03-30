13e420fcf259e2ae1cc6b54294d1a2df155d4222
Wesley Haws <Wes@Wess-MBP.app.byu.edu>
2016-03-29 22:01:51 -0600

    fix

--- laravel/public/demeter/elements/dem-db-info/dem-db-info.js
+++ laravel/public/demeter/elements/dem-db-info/dem-db-info.js
@@ -42,7 +42,7 @@ Polymer({
 		if(this.database.USERS == null || this.database.USERS.length < 1) {
 			this.push("database.USERS",{'name':'No Users'});
 		}
-		document.getElementById("dataInstanceInfo").innerHTML = "<b>Instance IP:</b> "+database.INSTANCEIP+"<br/><b>Instance Port:</b> "+database.INSTANCEPORT+"<br/>";
+		document.getElementById("dataInstanceInfo").innerHTML = "<b>Instance IP:</b> "+this.database.INSTANCEIP+"<br/><b>Instance Port:</b> "+this.database.INSTANCEPORT+"<br/>";
 	},
 	//-----------------------------------------------------
 	goToEdit:function() {
