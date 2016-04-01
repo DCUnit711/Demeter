8a50b3c2ee3bbbbef0e91d983cfe1bdd26cefbd4
Wesley Haws <Wes@Wess-MacBook-Pro.local>
2016-03-31 21:29:08 -0600

    testing.

--- laravel/public/demeter/elements/dem-home/dem-home.js
+++ laravel/public/demeter/elements/dem-home/dem-home.js
@@ -118,9 +118,10 @@ Polymer({
 											'INSTANCEPORT':this.databases[i].port,
 											'INSTANCEIP':this.databases[i].ipAddr});
 			}
+			Polymer.dom(this).node.__data__.databaseList[0].INSTANCEIP = this.databases[i].ipAddr;
 		}
 		console.log(this.databaseList);
-		Polymer.dom(this).node.__data__.databaseList[0].INSTANCEIP = this.databases[i].ipAddr;
+		// Polymer.dom(this).node.__data__.databaseList[0].INSTANCEIP = this.databases[i].ipAddr;
 		// this.$.listDatabase.render();
 	}
 });
